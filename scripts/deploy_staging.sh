2e42fa1c4d244163d4afcc9bd30932a249a26042
asansuri <abigasansuri@gmail.com>
2025-04-11 12:45:36 +0000

    Update deploy scripts for staging

--- scripts/deploy_staging.sh
+++ scripts/deploy_staging.sh
@@ -4,7 +4,19 @@
 echo "Deploying to staging..."
 
 scp app.py jenkins_slave@ip-172-31-5-113:/home/jenkins_slave/staging/ 
-ssh jenkins_slave@ip-172-31-5-113 'sudo systemctl restart flask-staging' 
+
+ssh jenkins_slave@ip-172-31-5-113<< 'EOF'
+
+    pkill -f "python3 app.py"
+
+    nohup python3 /var/www/staging/app.py > /var/www/staging/app.log 2>&1 &
+
+EOF
+
+
+
+
+
 
 
 
