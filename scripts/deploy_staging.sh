85e29d52b52e9c63638ef2921e7de547f5ed2778
ABIGA SANSURI.M <71253169+abigasansuri-1806@users.noreply.github.com>
2025-04-11 18:43:30 +0530

    Update deploy_staging.sh

--- scripts/deploy_staging.sh
+++ scripts/deploy_staging.sh
@@ -1,22 +1,16 @@
-
 #!/bin/bash
 
 echo "Deploying to staging..."
 
 scp app.py jenkins_slave@ip-172-31-5-113:/home/jenkins_slave/staging/ 
 
-ssh jenkins_slave@ip-172-31-5-113<< 'EOF'
+ssh jenkins_slave@ip-172-31-5-113 << 'EOF'
 
+    echo "Stopping existing Flask app..."
     pkill -f "python3 app.py"
 
+    echo "Starting new Flask app on port 5000..."
+    export FLASK_PORT=5000
     nohup python3 /home/jenkins_slave/staging/app.py > /home/jenkins_slave/staging/app.log 2>&1 &
 
 EOF
-
-
-
-
-
-
-
-
