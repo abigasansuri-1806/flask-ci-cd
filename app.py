493a17a8ad887935b477b1ba928780e7ddc87e0e
ABIGA SANSURI.M <71253169+abigasansuri-1806@users.noreply.github.com>
2025-04-11 18:42:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,12 @@
-
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
-
-
 @app.route('/')
-
 def home():
-
     return "Hello from Flask CI/CD Pipeline!"
 
-
-
 if __name__ == '__main__':
-
-    app.run(host='0.0.0.0', port=5000)
-
+    port = int(os.environ.get("FLASK_PORT", 5000))  # default to 5000 if not set
+    app.run(host='0.0.0.0', port=port)
